40a4015dc13ce58823683e5ff17612587b319e08
Nakell Williams <nakellwilliams@FBK-Mac.lan1>
2018-07-31 21:16:19 -0400

    initial

---
+++ assets/javascript/app.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+//decide variables you might need for this assignment
+//questions data type is arrayof objects
+var questions = 
+[
+  {
+    question: "What is the capital of United Kingdom?",
+    choices: ["Manchester", "Birmingham", "London", "Birmingham"],
+    answer: 2
+  },
+  {
+    question: "What is the capital of United States?",
+    choices: ["California", "New York", "Miami", "Florida"],
+    answer: 1
+  },
+  {
+    question: "What is the capital of United Kingdom?",
+    choices: ["Manchester", "Birmingham", "London", "Birmingham"],
+    answer: 2
+  },
+  {
+    question: "What is the capital of United States?",
+    choices: ["California", "New York", "Miami", "Florida"],
+    answer: 1
+  },
+  {
+    question: "What is the capital of United Kingdom?",
+    choices: ["Manchester", "Birmingham", "London", "Birmingham"],
+    answer: 2
+  },
+  {
+    question: "What is the capital of United States?",
+    choices: ["California", "New York", "Miami", "Florida"],
+    answer: 1
+  },
+  {
+    question: "What is the capital of United Kingdom?",
+    choices: ["Manchester", "Birmingham", "London", "Birmingham"],
+    answer: 2
+  },
+  {
+    question: "What is the capital of United States?",
+    choices: ["California", "New York", "Miami", "Florida"],
+    answer: 1
+  },
+  {
+    question: "What is the capital of United Kingdom?",
+    choices: ["Manchester", "Birmingham", "London", "Birmingham"],
+    answer: 2
+  },
+  {
+    question: "What is the capital of United States?",
+    choices: ["California", "New York", "Miami", "Florida"],
+    answer: 1
+  },
+  
+];
+
+
+var time=30;
+var correctanswers;
+var wronganswers;
+var unanswered;
+//var selections=[];
+//click on start
+//start timer and it should decrease by 1sec;
+//grab users choices
+//if timers endsup you need to endgame and check answers and display score 
+//else on click of submit do the same 
+//reset button and start new game
+});
